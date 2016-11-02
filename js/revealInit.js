4f7e52941c8dccbe747ee688e7168c9e6e32abaa
bechir <bechir@accessnow.org>
2016-11-02 01:03:22 +0100

    poc

---
+++ js/revealInit.js
@@ -0,0 +1,19 @@
+Reveal.initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  transition: 'slide',
+  dependencies: [
+    {src: 'components/moz-reveal.js/lib/js/classList.js', condition: function () {
+        return !document.body.classList;
+      }},
+    {src: 'components/moz-reveal.js/plugin/highlight/highlight.js', async: true, condition: function () {
+        return !!document.querySelector('pre code');
+      }, callback: function () {
+        hljs.initHighlightingOnLoad();
+      }},
+    {src: 'components/moz-reveal.js/plugin/zoom-js/zoom.js', async: true},
+    {src: 'components/moz-reveal.js/plugin/notes/notes.js', async: true}
+  ]
+});
